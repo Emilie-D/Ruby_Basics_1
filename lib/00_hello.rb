c9e389d8a5d949e9270b8fbdd641527b281defac
europeos <ryanzidago@gmail.com>
2019-04-03 17:23:48 +0200

    added hello.rb

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,28 +1,11 @@
-def say_hello
-	puts "Hello"
-end
-
-#say_hello
-
 def say_hello(first_name)
-	puts "Hello #{first_name}"
+  puts "Bonjour #{first_name}!"
 end
 
-#say_hello("Emilie")
-
-def ask_first_name
-	puts "Quel est ton nom ?"
-	print ">"
-	first_name = gets.chomp
-	return first_name
+def ask_first_name()
+  puts "What is your first name?"
+  first_name = gets.chomp.capitalize!
+  return first_name
 end
 
-#ask_first_name
-
-def perform
-	first_name = ask_first_name
-	say_hello(first_name)
-end
-
-perform
-
+say_hello(ask_first_name)
