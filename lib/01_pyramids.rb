7fb97f46412af92203201d62592e18ce2606ccf2
europeos <ryanzidago@gmail.com>
2019-04-04 13:43:09 +0200

    corrected mistake with full wtf pyramid

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,55 +1,51 @@
-def half_pyramid 
-	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étage veux-tu ?"
-	print ">"
-	user_number = gets.chomp.to_i
-	puts "Voici la pyramide :"
-	i = 0
-	while i < user_number
-  		i += 1
-  	puts " " * (user_number - i) + "#" * i
-	end 
+def half_pyramid()
+  puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+  $height = gets.chomp.to_i
+  puts
+  puts "Voici la pyramide :"
+  puts
+  $height.times { | x |
+    print ' ' * ($height - x)
+    puts '#' * (1 + x)
+  }
 end
 
-#half_pyramid
-
-def full_pyramid 
-	#puts "Salut, bienvenue dans ma super pyramide ! Combien d'étage veux-tu ?"
-	#print ">"
-	#user_number = gets.chomp.to_i
-	puts "Voici la pyramide :"
-	i = 0
-	while i < $user_number
-		i += 1
-	puts " " * ($user_number - i) + "#" * (2 * i - 1)
-	end
+def full_pyramid
+  puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+  height = gets.chomp.to_i
+  puts
+  puts "Voici la pyramide :"
+  puts
+  height.times { | x |
+    print ' ' * (height - x), '#' * (x)
+    puts '#' * (1 + x)
+  }
 end
 
-#full_pyramid
-
-def reverse_pyramid
-	#puts "Salut, bienvenue dans ma super pyramide ! Combien d'étage veux-tu ?"
-	#print ">"
-	#user_number = gets.chomp.to_i
-	#puts "Voici la pyramide :"
-	i = $user_number
-	while i > 1
-		i -= 1
-	puts " " * ($user_number - i) + "#" * (2 * i - 1) 
-	end
-end
-
-#reverse_pyramid
 
 def wtf_pyramid
-	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étage veux-tu ?"
-	print ">"
-	$user_number = gets.chomp.to_i
-		if $user_number %2 != 0 
-	puts "Seulement un nombre pair !"
-		else
-		full_pyramid
-		reverse_pyramid
-		end
+  puts "Salut, bienvenue dans ma super pyramide ! Combien d'étage veux-tu ?"
+  print ">"
+  x = gets.chomp.to_i
+  user_number = (x/2) + 1
+  puts "Voici la pyramide :"
+  i = 0
+  if x %2 != 0
+    while i < user_number
+      i += 1
+      puts " " * (user_number - i) + "#" * (2 * i - 1)
+    end
+    while i > 1
+      i -= 1
+      puts " " * (user_number - i) + "#" * (2 * i - 1)
+    end
+  else
+    puts "On accepte seulement les nombres impairs."
+  end
 end
 
+half_pyramid
+puts
+full_pyramid
+puts
 wtf_pyramid
